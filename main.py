9310381f614b2545cce06162d73646dbfd238826
SilenyProgramator <joel.hardonus@gmail.com>
2025-01-17 21:51:37 +0100

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from microbit import *
+
+
 def nacitani():
     basic.show_leds("""
                 . . . . .
@@ -43,10 +46,3 @@ def nacitani():
                     """)
                 
 nacitani()
-
-def on_button_pressed_a():
-    basic.show_string("Hitler")
-input.on_button_pressed(Button.A, on_button_pressed_a)
-def on_button_pressed_b():
-    basic.show_string("Lenin")
-input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -44,9 +44,3 @@ function nacitani() {
 }
 
 nacitani()
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    basic.showString("Hitler")
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    basic.showString("Lenin")
-})
